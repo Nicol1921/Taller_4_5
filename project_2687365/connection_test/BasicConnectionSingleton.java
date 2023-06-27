31ff9f8218754bc8b067ce7eaa16d79bdb0b4042
Nicol1921 <128154864+Nicol1921@users.noreply.github.com>
2023-06-27 14:42:35 -0500

    Add files via upload

---
+++ project_2687365/connection_test/BasicConnectionSingleton.java
@@ -0,0 +1,18 @@
+package co.edu.sena.project_2687365.connection_test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BasicConnectionSingleton {
+    private static String url = "jdbc:mysql://localhost:3306/my_app?serverTimezone=America/Bogota";
+    private static String user = "root";
+    private static String pass = "";
+    private static Connection conn;
+    public static Connection getInstance()
+            throws SQLException {
+        if (conn == null) {
+            conn = DriverManager.getConnection(url, user, pass);
+        }
+        return conn;
+    }
+} // BasicConnectionSingleton
---
+++ project_2687365/connection_test/UseBasicConnectionSingleton.java
@@ -0,0 +1,31 @@
+package co.edu.sena.project_2687365.connection_test;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UseBasicConnectionSingleton {
+    public static void main(String[] args) throws
+            SQLException {
+        try (Connection conn =
+                     BasicConnectionSingleton.getInstance();
+             Statement stmt =
+                     conn.createStatement();
+             ResultSet rs =
+                     stmt.executeQuery("SELECT * FROM my_app.users_tbl")) {while (rs.next()) {
+
+                System.out.print(rs.getString("user_firstname")
+                );
+                System.out.print(" | ");
+
+                System.out.print(rs.getString("user_lastname"));
+                System.out.print(" | ");
+                System.out.print(rs.getString("user_email"));
+                System.out.print(" | ");
+                System.out.print(rs.getString("user_password"));
+
+
+            }
+        } // end try
+    }
+} // UseBasicConnectionSingleton
